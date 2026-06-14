2c4bd264350a6bbb8bebbec964467c4ea4464f5f
yeshparashar31 <yash31parashar@gmail.com>
2026-06-14 23:06:22 +0530

    Update ValidAnagram.java

---
+++ Hashing/ValidAnagram.java
@@ -0,0 +1,32 @@
+/*
+LeetCode 242 - Valid Anagram
+
+Pattern: Hashing
+
+Time Complexity: O(n)
+Space Complexity: O(1)
+*/
+
+class Solution {
+    public boolean isAnagram(String s, String t) {
+
+        if(s.length() != t.length()) {
+            return false;
+        }
+
+        int[] count = new int[26];
+
+        for(int i = 0; i < s.length(); i++) {
+            count[s.charAt(i) - 'a']++;
+            count[t.charAt(i) - 'a']--;
+        }
+
+        for(int num : count) {
+            if(num != 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
