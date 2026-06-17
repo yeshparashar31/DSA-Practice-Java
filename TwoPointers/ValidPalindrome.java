4f2ee403f2b70a10b289d307369ac211a1f17386
yeshparashar31 <yash31parashar@gmail.com>
2026-06-17 23:48:37 +0530

    Create ValidPalindrome.java

---
+++ TwoPointers/ValidPalindrome.java
@@ -0,0 +1,40 @@
+/*
+LeetCode 125 - Valid Palindrome
+
+Pattern: Two Pointers
+
+Time Complexity: O(n)
+Space Complexity: O(1)
+*/
+
+class Solution {
+    public boolean isPalindrome(String s) {
+
+        int left = 0;
+        int right = s.length() - 1;
+
+        while(left < right)
+        {
+            while(left < right && !Character.isLetterOrDigit(s.charAt(left)))
+            {
+                left++;
+            }
+
+            while(left < right && !Character.isLetterOrDigit(s.charAt(right)))
+            {
+                right--;
+            }
+
+            if(Character.toLowerCase(s.charAt(left))
+               != Character.toLowerCase(s.charAt(right)))
+            {
+                return false;
+            }
+
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+}
