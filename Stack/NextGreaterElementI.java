3e015c43986a1b600b51e007a06a8a5aae7026ff
yeshparashar31 <yash31parashar@gmail.com>
2026-06-18 23:42:04 +0530

    Create NextGreaterElementI.java

---
+++ Stack/NextGreaterElementI.java
@@ -0,0 +1,38 @@
+/*
+LeetCode 496 - Next Greater Element I
+
+Pattern: Monotonic Stack
+
+Time Complexity: O(n + m)
+Space Complexity: O(n)
+*/
+
+import java.util.*;
+
+class Solution {
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+
+        HashMap<Integer,Integer> map = new HashMap<>();
+        Stack<Integer> stack = new Stack<>();
+
+        for(int num : nums2)
+        {
+            while(!stack.isEmpty() &&
+                  stack.peek() < num)
+            {
+                map.put(stack.pop(), num);
+            }
+
+            stack.push(num);
+        }
+
+        int[] ans = new int[nums1.length];
+
+        for(int i = 0; i < nums1.length; i++)
+        {
+            ans[i] = map.getOrDefault(nums1[i], -1);
+        }
+
+        return ans;
+    }
+}
