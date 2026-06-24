f7a2b168142df5f2f143491fde7f6887ad25a6e1
yeshparashar31 <yash31parashar@gmail.com>
2026-06-24 23:56:04 +0530

    Create Linked List Cycle II.java

---
+++ STACK/Linked List Cycle II.java
@@ -0,0 +1,38 @@
+/*
+LeetCode 142 - Linked List Cycle II
+
+Pattern: Floyd Cycle Detection
+
+Time Complexity: O(n)
+Space Complexity: O(1)
+*/
+
+public class Solution {
+
+    public ListNode detectCycle(ListNode head) {
+
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while(fast != null && fast.next != null)
+        {
+            slow = slow.next;
+            fast = fast.next.next;
+
+            if(slow == fast)
+            {
+                ListNode entry = head;
+
+                while(entry != slow)
+                {
+                    entry = entry.next;
+                    slow = slow.next;
+                }
+
+                return entry;
+            }
+        }
+
+        return null;
+    }
+}
