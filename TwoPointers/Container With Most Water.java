9fc2ed7672f89b7db0525bf888c696f0bc8802e4
Yesh Parashar <yash31parashar@gmail.com>
2026-06-30 23:27:00 +0530

    Create Container With Most Water.java

---
+++ TwoPointers/Container With Most Water.java
@@ -0,0 +1,33 @@
+//   [11]:- Container With Most Water
+
+
+class Solution {
+    public int maxArea(int[] height) {
+        
+        int left=0;
+        int right=height.length-1;
+        int maxArea=0;
+
+        while(left<right)
+        {
+            int width=right-left;
+
+            int minHeight=Math.min(height[left],height[right]);
+
+            int area=width*minHeight;
+
+            maxArea=Math.max(maxArea,area);
+
+            if(height[left]<height[right])
+            {
+                left++;
+            }
+            else
+            {
+                right--;
+            }
+
+        }
+        return maxArea;
+    }
+}
