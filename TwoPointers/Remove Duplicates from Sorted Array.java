56afa06745afa26afa5127661b6b02e40d632106
Yesh Parashar <yash31parashar@gmail.com>
2026-07-01 23:29:08 +0530

    Create  Remove Duplicates from Sorted Array.java

---
+++ TwoPointers/Remove Duplicates from Sorted Array.java
@@ -0,0 +1,21 @@
+// [26]:-  Remove Duplicates from Sorted Array
+
+
+
+
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        
+        int i=0;
+        for(int j=1;j<nums.length;j++)
+        {
+            if(nums[i]!=nums[j])
+            {
+                
+                nums[++i]=nums[j];
+            }
+        }
+
+        return i+1;
+    }
+}
